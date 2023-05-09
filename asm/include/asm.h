1a4d7765717255f02c75260b15e91829d56d9fc4
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-05-09 13:56:54 +0100

    [ADD] malloc check and made code cleaner

--- asm/include/asm.h
+++ asm/include/asm.h
@@ -5,15 +5,18 @@
 ** asm
 */
 
+#include <stdbool.h>
+
 #ifndef ASM_H
     #define ASM_H
 
     typedef struct label {
         char *name;
-        unsigned int ad;
+        long int ad;
     } label_t;
 
     typedef struct command {
+        char *line;
         char code_command;
         char coding_byte;
         char *parameters;
@@ -24,5 +27,10 @@
         int size;
         int (*ptr)(char *, char *, int);
     } arg_type_t;
+
+    int add_register(char *dest, char *arg, int i);
+    int add_direct(char *dest, char *arg, int i);
+    int add_indirect(char *dest, char *arg, int i);
+    bool line_to_command(command_t *com, int index);
 
 #endif /* ASM_H */
--- asm/src/command_to_data.c
+++ asm/src/command_to_data.c
@@ -6,62 +6,14 @@
 */
 
 char **turn_str_to_word_array(char const *str, char sep);
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdlib.h>
 #include <unistd.h>
-#include <math.h>
 #include <string.h>
 #include "asm.h"
 #include "op.h"
-
-void add_int(char *dest, long int nb, int i, int delta)
-{
-    long int order = 0;
-    int cond = 0;
-
-    order = pow(2, delta * 8);
-    for (int x = i; x < i + delta; x++) {
-        for (int y = 0; y < 8; y++) {
-            cond = (nb & order);
-            dest[x] += cond;
-            nb -= cond;
-            order /= 2;
-        }
-    }
-    cond = (nb & order);
-    dest[i + delta - 1] += cond;
-    nb >>= (cond) ? 1 : 0;
-}
-
-int add_register(char *dest, char *arg, int i)
-{
-    int reg = 0;
-    int order = 0;
-
-    reg = atoi(&arg[1]);
-    add_int(dest, reg, i, REG_SIZE);
-    return i + REG_SIZE;
-}
-
-int add_direct(char *dest, char *arg, int i)
-{
-    int dir = 0;
-    int order = 0;
-
-    dir = atoi(&arg[1]);
-    add_int(dest, dir, i, DIR_SIZE);
-    return i + DIR_SIZE;
-}
-
-int add_indirect(char *dest, char *arg, int i)
-{
-    int ind = 0;
-    int order = 0;
-
-    ind = atoi(arg);
-    add_int(dest, ind, i, IND_SIZE);
-    return i + IND_SIZE;
-}
+#include "herror.h"
 
 static const arg_type_t arg_type[3] = {
     {REG_SIZE, add_register},
@@ -74,6 +26,7 @@ char *get_type(char **arg, int index)
     char *type = NULL;
 
     type = malloc(op_tab[index].nbr_args * sizeof(char));
+    ASSERT_MALLOC(type, NULL);
     for (int i = 1; arg[i]; i++) {
         if (arg[i][0] == 'r') {
             type[i - 1] = 0b1;
@@ -94,13 +47,14 @@ char *get_param(int param_size, char *type, char **arg, int index)
     int mv = 0;
 
     new = malloc(param_size * sizeof(char));
+    ASSERT_MALLOC(new, NULL);
     memset(new, 0, param_size);
     for (int i = 0; i < op_tab[index].nbr_args; i++)
         mv = arg_type[type[i] - 1].ptr(new, arg[i + 1], mv);
     return new;
 }
 
-char make_coding_byte(command_t *new, char *type, int index)
+void make_coding_byte(command_t *new, char *type, int index)
 {
     new->coding_byte = 0;
     new->param_size = 0;
@@ -113,30 +67,18 @@ char make_coding_byte(command_t *new, char *type, int index)
         new->coding_byte <<= 2;
 }
 
-command_t *line_to_command(char *line, int index)
+bool line_to_command(command_t *com, int index)
 {
-    command_t *new = NULL;
     char **arg = NULL;
     char *type = NULL;
-    int mv = 0;
 
-    arg = turn_str_to_word_array(line, ' ');
-    new = malloc(sizeof(command_t));
-    new->code_command = op_tab[index].code;
+    arg = turn_str_to_word_array(com->line, ' ');
+    ASSERT_MALLOC(arg, false);
+    com->code_command = op_tab[index].code;
     type = get_type(arg, index);
-    make_coding_byte(new, type, index);
-    new->parameters = get_param(new->param_size, type, arg, index);
-    return new;
-}
-
-int main(void)
-{
-    header_t head = {0};
-    command_t *new = NULL;
-
-    new = line_to_command("ld %0, r3", 1);
-    write(1, &new->code_command, sizeof(char));
-    write(1, &new->coding_byte, sizeof(char));
-    write(1, new->parameters, new->param_size);
-    return 0;
+    ASSERT_MALLOC(type, false);
+    make_coding_byte(com, type, index);
+    com->parameters = get_param(com->param_size, type, arg, index);
+    ASSERT_MALLOC(com->parameters, false);
+    return true;
 }
