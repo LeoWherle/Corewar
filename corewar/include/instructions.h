ec152faa0e8b64a3e49e4c9d789f98b705354fb4
Léo Rentmeister <leo.wehrle@epitech.eu>
2023-05-13 00:42:11 +0200

    [REMOVE] .gitkeep files

---
+++ corewar/include/instructions.h
@@ -0,0 +1,46 @@
+/*
+** EPITECH PROJECT, 2023
+** corewar [WSL: fedora]
+** File description:
+** instructions
+*/
+
+#ifndef INSTRUCTIONS_H_
+    #define INSTRUCTIONS_H_
+
+    #include "op.h"
+    #include "vm.h"
+
+    // used to get the instruction from the bytecode
+    typedef struct instruction_s {
+        command_t command;
+    } instruction_t;
+
+    /*
+    ** HANDLERS
+    */
+    // get the instruction from the bytecode (static const instructions _t i[])
+    void get_cmd(process_t *process, char bytecode);
+
+    /*
+    **  INSTRUCTIONS
+    */
+
+    void cmd_live(vm_t *vm, process_t *process);
+    void cmd_ld(vm_t *vm, process_t *process);
+    void cmd_st(vm_t *vm, process_t *process);
+    void cmd_add(vm_t *vm, process_t *process);
+    void cmd_sub(vm_t *vm, process_t *process);
+    void cmd_and(vm_t *vm, process_t *process);
+    void cmd_or(vm_t *vm, process_t *process);
+    void cmd_xor(vm_t *vm, process_t *process);
+    void cmd_zjmp(vm_t *vm, process_t *process);
+    void cmd_ldi(vm_t *vm, process_t *process);
+    void cmd_sti(vm_t *vm, process_t *process);
+    void cmd_fork(vm_t *vm, process_t *process);
+    void cmd_lld(vm_t *vm, process_t *process);
+    void cmd_lldi(vm_t *vm, process_t *process);
+    void cmd_lfork(vm_t *vm, process_t *process);
+    void cmd_aff(vm_t *vm, process_t *process);
+
+#endif /* !INSTRUCTIONS_H_ */
