4d50fc8e6821f0f96b372ac57d552a5ccc39cf5a
Theodore <theodore.magna@epitech.eu>
2023-05-15 20:07:59 +0200

    [FIX] remove personnal main

--- corewar/src/main.c
+++ corewar/src/main.c
@@ -10,14 +10,26 @@
 #include <string.h>
 #include <unistd.h>
 #include "clist.h"
-#include "args.h"
 
-int main(int ac, char *av[])
+void print_string(any_t data)
 {
-    args_t *args = malloc(sizeof(args_t));
+    write(1, data, strlen(data));
+}
 
-    args = get_args(ac, av, args);
-    if (args == NULL)
-        return 84;
+void destroy_stack(UNUSED any_t data)
+{
+    return;
+}
+
+int main(void)
+{
+    list_t *list = list_init();
+
+    list->interface->append(list, "Hello");
+    list->interface->append(list, "World");
+    list->interface->append(list, "!");
+    list->interface->print(list, &print_string, " ");
+    write(1, "\n", 1);
+    list->interface->destroy(list, &destroy_stack);
     return 0;
 }
