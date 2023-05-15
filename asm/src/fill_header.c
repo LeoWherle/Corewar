a46e7e6b4521e3faf5facfa7a35ee5a5ef322b9d
Martin Sabatier <martin.sabatier@epitech.eu>
2023-05-15 13:23:50 +0200

    [FIX] header parser error handling

--- asm/src/fill_header.c
+++ asm/src/fill_header.c
@@ -7,6 +7,7 @@
 
 #include <stdbool.h>
 #include <stddef.h>
+#include <stdlib.h>
 #include <stdio.h>
 #include "clist.h"
 #include "mystr.h"
@@ -44,6 +45,35 @@ static bool get_comment(header_t *header, char *line)
     return true;
 }
 
+bool check_instruction(char *line, int got_name, int got_comment)
+{
+    if (line[0] == '\n' || line[0] == COMMENT_CHAR)
+        return true;
+    if (my_strncmp(line, NAME_CMD_STRING, 4) == 0 && got_name)
+        return false;
+    if (my_strncmp(line, COMMENT_CMD_STRING, 7) == 0 && got_comment)
+        return false;
+    if (my_strncmp(line, NAME_CMD_STRING, 4) == 0 || my_strncmp(line,
+        COMMENT_CMD_STRING, 7) == 0)
+        return true;
+    return false;
+}
+
+char *clear_line(char *line)
+{
+    char *new_line = NULL;
+    int in = 0;
+
+    new_line = malloc(sizeof(char) * strlen(line) + 1);
+    ASSERT_MALLOC(new_line, NULL);
+    while (line[in] == ' ' || line[in] == '\t' || line[in] == '\0')
+        in++;
+    for (int i = 0; line[in] != '\0'; i++, in++)
+        new_line[i] = line[in];
+    free(line);
+    return new_line;
+}
+
 int header_parser(header_t *header, FILE *fd)
 {
     size_t size = 0;
@@ -52,14 +82,15 @@ int header_parser(header_t *header, FILE *fd)
     int got_comment = false;
 
     header->magic = COREWAR_EXEC_MAGIC;
-    header->prog_size = 0;
-    while (getline(&line, &size, fd) != -1 && (!got_comment || !got_name)) {
-        if (line[0] != '\n' && line[0] != COMMENT_CHAR && line[0] != '.')
+    while (getline(&line, &size, fd) != -1) {
+        line = clear_line(line);
+        if (line[0] != COMMENT_CHAR && line[0] != '.' && line[0] != '\n')
+            break;
+        if (!check_instruction(line, got_name, got_comment))
             return 84;
-        if (strncmp(line, NAME_CMD_STRING, 5) == 0)
+        if (my_strncmp(line, NAME_CMD_STRING, 4) == 0)
             got_name = get_name(header, line);
-        if (strncmp(line, COMMENT_CMD_STRING, 8) == 0 &&
-            !got_comment && got_name)
+        if (my_strncmp(line, COMMENT_CMD_STRING, 7) == 0 && got_name)
             got_comment = get_comment(header, line);
     }
     if (!got_name || !got_comment)
