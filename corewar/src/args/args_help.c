b74c81dca879ad470a411d6f17f7835dce1cf5ce
Theodore <theodore.magna@epitech.eu>
2023-05-16 14:57:47 +0200

    [FIX] replace weird ascii in helper to simple ones

--- corewar/src/args/args_help.c
+++ corewar/src/args/args_help.c
@@ -15,12 +15,12 @@ static void print_helper(void)
     write(1, " [-a load_address] prog_name] ...\n", 34);
     write(1, "DESCRIPTION\n", 12);
     write(1, "\t-dump nbr_cycle dumps the memory after the nbr_cycle", 52);
-    write(1, " execution (if the round isn’t already over) with the", 52);
+    write(1, " execution (if the round isn't already over) with the", 52);
     write(1, " following format: 32 bytes/line\n", 33);
     write(1, "\tin hexadecimal (A0BCDEFE1DD3...)\n", 34);
-    write(1, "\t-n prog_number sets the next program’s number. By", 49);
+    write(1, "\t-n prog_number sets the next program's number. By", 49);
     write(1, " default, the first free number in the parameter order\n", 55);
-    write(1, "\t-a load_address sets the next program’s loading", 49);
+    write(1, "\t-a load_address sets the next program's loading", 49);
     write(1, " address. When no address is specified, optimize the", 51);
     write(1, " addresses so that the processes are as far away from", 52);
     write(1, " each other as possible. The addresses are MEM_SIZE", 50);
