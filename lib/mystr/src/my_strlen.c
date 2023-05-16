14626474301ad5dc6df5eb03814f8931d1d8cc92
Theodore <theodore.magna@epitech.eu>
2023-05-16 16:42:25 +0200

    [EDIT] better strlen

--- lib/mystr/src/my_strlen.c
+++ lib/mystr/src/my_strlen.c
@@ -7,11 +7,11 @@
 
 int my_strlen(char const *str)
 {
-    int index = 0;
+    char const *start = str;
 
     if (!str)
         return (0);
-    while (str[index] != '\0')
-        index++;
-    return (index);
+    while (*str)
+        str++;
+    return (str - start);
 }
