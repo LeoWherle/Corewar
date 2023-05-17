9f9fe15a8058a66708a032439bb5547c4a604793
Léo Rentmeister <leo.wehrle@epitech.eu>
2023-05-17 10:53:05 +0200

    [ADD] basic initialisation of champion

--- corewar/src/champions/champion_init.c
+++ corewar/src/champions/champion_init.c
@@ -9,8 +9,13 @@
 #include "vm.h"
 #include "herror.h"
 
-static void champion_init(champion_t *vm)
+static void champion_init(champion_t *champion)
 {
+    champion->id = 0;
+    champion->alive = false;
+    champion->file_path = NULL;
+    champion->load_address = 0;
+    champion->load_address_value = 0;
     return;
 }
 
