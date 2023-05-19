d3e21dec68a335ed4e6daa73996a4dc8db61e2a6
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-05-19 14:01:55 +0100

    [ADD] free of memory when errors

--- asm/src/asm_main.c
+++ asm/src/asm_main.c
@@ -35,27 +35,29 @@ void free_command(void *ptr)
     free(command);
 }
 
-int compile_and_print(list_t *com_list, list_t *label_list,
-                    header_t *header, char **av)
-{
-    if (!search_in_command(com_list, label_list))
-        return 84;
-    for (node_t *node = com_list->head; node; node = node->next) {
-        if (!line_to_command(node->data)) {
-            return 84;
-        }
-    }
-    if (!print_comp(com_list, header, av[1])) {
-        return 84;
-    }
-    return 0;
-}
-
-void free_list(list_t *com_list, list_t *label_list)
+static int free_list(list_t *com_list, list_t *label_list, int r)
 {
     free(label_list->interface);
     list_destroy(label_list, free_label);
     list_destroy(com_list, free_command);
+    return r;
+}
+
+int compile_and_print(list_t *com_list, list_t *label_list,
+                    header_t *header, char **av)
+{
+    if (!search_in_command(com_list, label_list)) {
+        return free_list(com_list, label_list, 84);
+    }
+    for (node_t *node = com_list->head; node; node = node->next) {
+        if (!line_to_command(node->data)) {
+            return free_list(com_list, label_list, 84);;
+        }
+    }
+    if (!print_comp(com_list, header, av[1])) {
+        return free_list(com_list, label_list, 84);
+    }
+    return free_list(com_list, label_list, 0);
 }
 
 int main(int ac, char **av)
@@ -64,20 +66,19 @@ int main(int ac, char **av)
     list_t *label_list = list_init();
     header_t header = {0};
     FILE *fd = NULL;
-    int r = 0;
 
-    if (ac != 2) return 84;
+    if (ac != 2)
+        return free_list(com_list, label_list, 84);
     fd = fopen(av[1], "r");
-    ASSERT_PTR(fd, 84);
+    if (!fd)
+        return free_list(com_list, label_list, 84);
     header.magic = COREWAR_EXEC_MAGIC;
     header.prog_size = 0;
     if (header_parser(&header, fd) == 84)
-        return 84;
+        return free_list(com_list, label_list, 84);
     fseek(fd, 0, SEEK_SET);
     if (code_parser(&header, fd, com_list, label_list) == 84)
-        return 84;
+        return free_list(com_list, label_list, 84);
     fclose(fd);
-    r = compile_and_print(com_list, label_list, &header, av);
-    free_list(com_list, label_list);
-    return r;
+    return compile_and_print(com_list, label_list, &header, av);
 }
