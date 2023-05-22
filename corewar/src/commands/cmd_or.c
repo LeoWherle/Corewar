4943cf934db89408bb5690d2702b5bdf0fae71db
Martin Sabatier <martin.sabatier@epitech.eu>
2023-05-22 18:01:24 +0200

    [ADD] and or and xor instructions

--- corewar/src/commands/cmd_or.c
+++ corewar/src/commands/cmd_or.c
@@ -11,7 +11,37 @@
 /*
 Similar to and, but performing a binary OR.
 */
+int or_to_reg(vm_t *vm, process_t *process, char *type, int *size)
+{
+    int first = 0;
+    int second = 0;
+    int tot = 0;
+
+    process->index++;
+    first = param_getter(process, vm, type[0], size[0]);
+    second = param_getter(process, vm, type[1], size[1]);
+    tot = first | second;
+    set_reg(process, vm, tot);
+    return tot;
+}
+
 void cmd_or(vm_t *vm, process_t *process)
 {
+    char coding_byte = '\0';
+    char command = '\0';
+    char *type = NULL;
+    int *size = NULL;
 
+    command = cbuffer_getb(vm->arena, process->index);
+    process->index++;
+    coding_byte = cbuffer_getb(vm->arena, process->index);
+    type = get_coding_byte(coding_byte);
+    size = get_size(type, command - 1);
+    if (command != 4 || !param_checker(type, command - 1)) {
+        kill_process(process, vm);
+    } else {
+        add_to_reg(vm, process, type, size);
+    }
+    free(type);
+    free(size);
 }
