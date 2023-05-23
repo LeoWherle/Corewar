7e7444c92dec8f9ef7f047f57d87581e38ac51c6
Martin Sabatier <martin.sabatier@epitech.eu>
2023-05-23 12:32:49 +0200

    [ADD] st and sti instructions

--- corewar/src/commands/cmd_st.c
+++ corewar/src/commands/cmd_st.c
@@ -15,7 +15,40 @@ the second (whether a register or a number).
 st r4,34 stores the content of r4 at the address PC + 34 % IDX_MOD.
 st r3,r8 copies the content of r3 into r8.
 */
+static void st_to_reg(vm_t *vm, process_t *process, char *type, int *size)
+{
+    int first = 0;
+    int second = 0;
+    int tot = 0;
+    int pos = 0;
+
+    pos = process->index;
+    process->index += 2;
+    first = param_getter(process, vm, type[0], size[0]);
+    if (type[1] == IND_CODE) {
+        second = param_getter(process, vm, type[1], size[1]);
+        set_mem(vm, pos + second % IDX_MOD, SWAP_INT32_SAFE(first), REG_SIZE);
+    } else {
+        set_reg(process, vm, tot);
+    }
+}
+
 void cmd_st(vm_t *vm, process_t *process)
 {
+    char coding_byte = '\0';
+    char command = '\0';
+    char *type = NULL;
+    int *size = NULL;
 
+    command = cbuffer_getb(vm->arena, process->index);
+    coding_byte = cbuffer_getb(vm->arena, process->index + 1);
+    type = get_coding_byte(coding_byte);
+    size = get_size(type, command - 1);
+    if (command != 4 || !param_checker(type, command - 1)) {
+        kill_process(process, vm);
+    } else {
+        st_to_reg(vm, process, type, size);
+    }
+    free(type);
+    free(size);
 }
--- corewar/src/commands/cmd_sti.c
+++ corewar/src/commands/cmd_sti.c
@@ -14,7 +14,43 @@ The first one must be a register.
 The other two can be indexes or registers.
 sti r2,%4,%5 copies the content of r2 into the address PC + (4+5)% IDX_MOD.
 */
+static void sti_to_reg(vm_t *vm, process_t *process, char *type, int *size)
+{
+    int first = 0;
+    int second = 0;
+    int tot = 0;
+    int pos = 0;
+    int reg = 0;
+
+    pos = process->index;
+    process->index += 2;
+    reg = param_getter(process, vm, type[0], size[0]);
+    first = param_getter(process, vm, type[1], size[1]);
+    second = param_getter(process, vm, type[2], size[2]);
+    if (type[1] == IND_CODE) {
+        first = cbuffer_geti(vm->arena, pos + first % IDX_MOD);
+    }
+    set_mem(vm, pos + (first + second) % IDX_MOD,
+    SWAP_INT32_SAFE(reg), REG_SIZE);
+}
+
 void cmd_sti(vm_t *vm, process_t *process)
 {
+    char coding_byte = '\0';
+    char command = '\0';
+    char *type = NULL;
+    int *size = NULL;
 
+    command = cbuffer_getb(vm->arena, process->index);
+    coding_byte = cbuffer_getb(vm->arena, process->index + 1);
+    type = get_coding_byte(coding_byte);
+    size = get_size(type, command - 1);
+    if (command != 11 || !param_checker(type, command - 1)) {
+        kill_process(process, vm);
+    } else {
+        st_to_reg(vm, process, type, size);
+    }
+    free(type);
+    free(size);
 }
+
