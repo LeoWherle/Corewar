cc7f5278eb981a0254c3c0cf4739555dac5418cf
Martin Sabatier <martin.sabatier@epitech.eu>
2023-05-23 11:01:55 +0200

    [ADD] ld and lld instructions

--- corewar/src/commands/cmd_ld.c
+++ corewar/src/commands/cmd_ld.c
@@ -16,7 +16,42 @@ which must be a register (not the PC). This operation modifies the carry.
 ld 34,r3 loads the REG_SIZE bytes starting
 at the address PC + 34 % IDX_MOD into r3.
 */
+int ld_to_reg(vm_t *vm, process_t *process, char *type, int *size)
+{
+    int value = 0;
+    int tot = 0;
+    int pos = 0;
+
+    pos = process->index;
+    process->index += 2;
+    value = param_getter(process, vm, type[0], size[0]);
+    if (type[0] == IND_CODE) {
+        tot = pos + value % IDX_MOD;
+        value = cbuffer_geti(vm->arena, tot);
+    }
+    if (value != 0) {
+        process->carry = 1;
+    }
+    set_reg(process, vm, value);
+    return value;
+}
+
 void cmd_ld(vm_t *vm, process_t *process)
 {
+    char coding_byte = '\0';
+    char command = '\0';
+    char *type = NULL;
+    int *size = NULL;
 
+    command = cbuffer_getb(vm->arena, process->index);
+    coding_byte = cbuffer_getb(vm->arena, process->index + 1);
+    type = get_coding_byte(coding_byte);
+    size = get_size(type, command - 1);
+    if (command != 2 || !param_checker(type, command - 1)) {
+        kill_process(process, vm);
+    } else {
+        ld_to_reg(vm, process, type, size);
+    }
+    free(type);
+    free(size);
 }
