37227cf3dbafa5a7cd17b24895e3cc9f35dcc3f9
Theodore <theodore.magna@epitech.eu>
2023-05-24 12:10:34 +0200

    [ADD] print to debug on debug mode

--- corewar/src/instructions/instruction_getter.c
+++ corewar/src/instructions/instruction_getter.c
@@ -53,6 +53,8 @@ static void instruction_get_process(any_t data, void *vm_ptr, UNUSED void *none)
         process->instruction = instructions[instruction_byte - 1].command;
         process->cycle_to_wait = instructions[instruction_byte - 1].cycles;
         process->exec = false;
+        DEBUGF("champ_id : %d instruction : %s\n",
+        process->champion->id, op_tab[instruction_byte - 1].mnemonique);
     }
 }
 
