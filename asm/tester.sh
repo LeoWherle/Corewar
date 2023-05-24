cff06b63389e88ff2ff3126cfd748f0478b7a6e1
Martin Sabatier <martin.sabatier@epitech.eu>
2023-05-24 14:40:47 +0200

    [ADD] tester.sh for asm and fixed struct pointer

---
+++ asm/tester.sh
@@ -0,0 +1,12 @@
+./asm ../assets/champions/src/abel.s
+./asm ../assets/champions/src/bill.s
+./asm ../assets/champions/src/tyron.s
+./asm ../assets/champions/src/pdd.s
+diff ../assets/champions/src/abel.cor ../assets/champions/abel.cor
+diff ../assets/champions/src/bill.cor ../assets/champions/bill.cor
+diff ../assets/champions/src/tyron.cor ../assets/champions/tyron.cor
+diff ../assets/champions/src/pdd.cor ../assets/champions/pdd.cor
+rm ../assets/champions/src/abel.cor
+rm ../assets/champions/src/bill.cor
+rm ../assets/champions/src/tyron.cor
+rm ../assets/champions/src/pdd.cor
