7bd6f36f8e901c131099d76fa8ea474941308d52
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-05-30 09:03:57 +0100

    [ADD] functional ntml.s

--- NTML.s
+++ NTML.s
@@ -5,14 +5,14 @@
                 sti r1, %:setup_end, %1
                 ld %0, r2
                 ld %1, r3
-                ld %10, r5
+                ld %11, r5
 loop:           live %69
                 add r2, r3, r2
                 sub r4, r2, r5
                 zjmp %:setup_end
                 fork %:loop
-                ld r6, %0
+                ld %0, r6
                 zjmp %:loop
 setup_end:      live %69
-                ld r6, %0
+                ld %0, r6
                 zjmp %:setup_end
