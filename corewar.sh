a37ea5128ef0ff19d88f68e572c234c50a0aabbf
leo rentmeister <leo.wehrle@epitech.eu>
2023-05-31 18:21:11 +0200

    [ADD] colors to shell script

--- corewar.sh
+++ corewar.sh
@@ -1,15 +1,25 @@
 #!/bin/bash
 
+BLACK="\033[0;30m"
+RED="\033[1;31m"
+GREEN="\033[0;32m"
+YELLOW="\033[0;33m"
+BLUE="\033[0;34m"
+MAGENTA="\033[1;35m"
+CYAN="\033[0;36m"
+WHITE="\033[1;37m"
+RESET="\033[0m"
+
 if [ $# \< 2 ]
 then
-    echo "not enough champions"
-    echo "USAGE"
-    echo "    ./corewar [-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] ...\n"
-    echo "DESCRIPTION"
-    echo "    -dump nbr_cycle dumps the memory after the nbr_cycle execution (if the round isn't already over) with the following format: 32 bytes/line"
+    printf $RED"Error:$RESET not enough champions\n"
+    printf $WHITE"USAGE\n"$RESET
+    printf "    ./corewar [$GREEN-dump$RESET nbr_cycle] $MAGENTA[$RESET[$GREEN-n$RESET prog_number] [$GREEN-a$RESET load_address] prog_name$MAGENTA]$RESET ...\n"
+    printf $WHITE"DESCRIPTION\n"$RESET
+    printf "   $GREEN -dump$RESET nbr_cycle dumps the memory after the nbr_cycle execution (if the round isn't already over) with the following format: 32 bytes/line\n"
     echo "    in hexadecimal (A0BCDEFE1DD3...)"
-    echo "    -n prog_number sets the next program's number. By default, the first free number in the parameter order"
-    echo "    -a load_address sets the next program's loading address. When no address is specified, optimize the addresses so that the processes are as far away from each other as possible. The addresses are MEM_SIZE modulo."
+    printf "   $GREEN -n$RESET prog_number sets the next program's number. By default, the first free number in the parameter order\n"
+    printf "   $GREEN -a$RESET load_address sets the next program's loading address. When no address is specified, optimize the addresses so that the processes are as far away from each other as possible. The addresses are MEM_SIZE modulo.\n"
 else
     theme_list=("NICO" "defaultstarwars" "ETIPECH" "FIGHTER" "MINECRAFT" "FF7")
 
