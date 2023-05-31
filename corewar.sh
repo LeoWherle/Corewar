41033ea9f4b19d09b1300972550060fce31c76dd
leo rentmeister <leo.wehrle@epitech.eu>
2023-05-31 15:59:49 +0200

    [ADD] Usage display

--- corewar.sh
+++ corewar.sh
@@ -3,6 +3,13 @@
 if [ $# \< 2 ]
 then
     echo "not enough champions"
+    echo "USAGE"
+    echo "    ./corewar [-dump nbr_cycle] [[-n prog_number] [-a load_address] prog_name] ...\n"
+    echo "DESCRIPTION"
+    echo "    -dump nbr_cycle dumps the memory after the nbr_cycle execution (if the round isn't already over) with the following format: 32 bytes/line"
+    echo "    in hexadecimal (A0BCDEFE1DD3...)"
+    echo "    -n prog_number sets the next program's number. By default, the first free number in the parameter order"
+    echo "    -a load_address sets the next program's loading address. When no address is specified, optimize the addresses so that the processes are as far away from each other as possible. The addresses are MEM_SIZE modulo."
 else
     theme_list=("NICO" "defaultstarwars" "ETIPECH" "FIGHTER" "MINECRAFT")
 
