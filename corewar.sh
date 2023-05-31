0f0aaedda5e02cf43de681289b72395e31bad09f
leo rentmeister <leo.wehrle@epitech.eu>
2023-05-31 14:01:39 +0200

    [ADD] check if enough arguments

--- corewar.sh
+++ corewar.sh
@@ -1,13 +1,18 @@
 #!/bin/bash
 
-theme_list=("NICO" "defaultstarwars" "ETIPECH" "FIGHTER" "MINECRAFT")
+if [ $# \< 2 ]
+then
+    echo "not enough champions"
+else
+    theme_list=("NICO" "defaultstarwars" "ETIPECH" "FIGHTER" "MINECRAFT")
 
-length=${#theme_list[@]}
-# generate a random index
-index=$((RANDOM % length))
+    length=${#theme_list[@]}
+    # generate a random index
+    index=$((RANDOM % length))
 
-# get a random string
-flag=${theme_list[index]}
+    # get a random string
+    flag=${theme_list[index]}
 
-make random-build THEME=-D\"$flag\"
-./bonus/bonus $@
+    make random-build THEME=-D\"$flag\"
+    ./bonus/bonus $@
+fi
