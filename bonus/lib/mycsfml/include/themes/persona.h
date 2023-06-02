ebacfd80f27a2ead99981ea218aeda9636106b49
Nathanael-Kimbembe <nathanael.kimbembe-croissant@epitech.eu>
2023-06-02 10:55:23 +0100

    [ADD] persona theme

---
+++ bonus/lib/mycsfml/include/themes/persona.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** graphic_corewar [WSL: fedora]
+** File description:
+** persona
+*/
+
+#ifdef PERSONA
+    #define LOGO "persona_logo.png"
+    #define MUSIC "Persona-5-Last-Surprise.ogg"
+    #define FONT_CHAMP "Expose-Regular.otf"
+    #define FONT_TEXT "Markin-LT-Ultra-Bold.ttf"
+    #define TEXT_COLOR sfColor_fromRGBA(255, 0, 0, 255)
+    #define BACKGROUND sfColor_fromRGBA(0, 0, 0, 255)
+#endif
