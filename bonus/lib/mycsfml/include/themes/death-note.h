103777cbe2b5f4fa84481ca9f2a4d8041de043c6
Klaiveft <pierre.bourgery@epitech.eu>
2023-06-05 17:20:11 +0200

    ADD: death note theme

---
+++ bonus/lib/mycsfml/include/themes/death-note.h
@@ -0,0 +1,15 @@
+/*
+** EPITECH PROJECT, 2023
+** graphic_corewar [WSL: fedora]
+** File description:
+** death-note
+*/
+
+#ifdef DEATH
+    #define LOGO "death (1).png"
+    #define MUSIC "ナイトメア NIGHTMARE - the WORLD [OFFICIAL MUSIC VIDEO].ogg"
+    #define CLEAR_COLOR sfColor_fromRGB(0, 0, 0)
+    #define FONT_TEXT "Death Note.ttf"
+    #define FONT_CHAMP "Death Note.ttf"
+    #define TEXT_COLOR sfColor_fromRGBA(255, 255, 255, 255)
+#endif
